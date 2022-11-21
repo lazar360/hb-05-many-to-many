aa719bc34f78cc298fec9cf641e53ea4c0f33965
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2022-11-21 09:28:27 +0100

    end commit

--- src/com/luv2code/hibernate/demo/entity/Student.java
+++ src/com/luv2code/hibernate/demo/entity/Student.java
@@ -1,10 +1,17 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 
@@ -25,6 +32,16 @@ public class Student {
 	
 	@Column(name="email")
 	private String email;
+	
+	@ManyToMany(fetch=FetchType.LAZY, 
+			cascade = {CascadeType.DETACH, CascadeType.MERGE, 
+			CascadeType.PERSIST, CascadeType.REFRESH})
+	@JoinTable(
+			name="course_student",
+			joinColumns=@JoinColumn(name="student_id"),
+			inverseJoinColumns=@JoinColumn(name="course_id")
+			)
+	private List<Course> courses;
 	
 	public Student() {
 	}
@@ -68,7 +85,15 @@ public class Student {
 		this.email = email;
 	}
 
-	 @Override
+	 public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	@Override
 	    public String toString() {
 	        return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 	                ;
